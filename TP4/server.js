d3b65ffa8a47556afed4dcca80fae9141ecb8aa2
jcm300 <zeclmartins@gmail.com>
2018-10-12 00:07:15 +0100

    website, server in node.js and manifesto added

---
+++ TP4/server.js
@@ -0,0 +1,24 @@
+var http = require('http')
+var fs = require('fs')
+var url = require('url')
+
+http.createServer((req, res) => {
+    var purl = url.parse(req.url, true)
+
+    if(purl.pathname=="/") var filePath = "./website/index.html"
+    else var filePath = "./website" + purl.pathname
+
+    fs.readFile(filePath, (erro, dados) => {
+        res.writeHead(200, { 'Content-Type': 'text/html' })
+
+        if(!erro)
+            res.write(dados)
+        else
+            res.write(erro + '')
+
+        res.end()
+    })
+
+}).listen(4001, () => {
+    console.log('Servidor à escuta na porta 4001...')
+})
