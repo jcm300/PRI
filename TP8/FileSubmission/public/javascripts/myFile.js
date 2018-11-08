755ed1e47d158633c74b0a6f3ffb9d15a5512399
jcm300 <zeclmartins@gmail.com>
2018-11-08 23:57:39 +0000

    TP8 done

---
+++ TP8/FileSubmission/public/javascripts/myFile.js
@@ -0,0 +1,33 @@
+$(() => {
+    $("#table").load("http://localhost:4008/table")
+    
+    $("#sendFile").click(e => {
+        e.preventDefault()
+        $("#table").append("<tr>\n<td><a href=\"http://localhost:4008/uploaded/"+ $("#files")[0].files[0].name + "\">" + $("#files")[0].files[0].name + "</a></td>\n<td>" + $("#desc").val() + "</td>\n</tr>")
+        ajaxPost()
+    })
+
+    function ajaxPost(){
+        var formData = new FormData()
+        formData.append("file",$("#files")[0].files[0])
+        formData.append("desc",$("#desc").val())
+        $.ajax({
+            type: "POST",
+            enctype: "form/multipart",
+            processData: false,
+            contentType: false,
+            url: "http://localhost:4008/saveFile",
+            data: formData,
+            success: n => {
+                if(n!="") alert("Ficheiro gravado com sucesso: " + n)
+                else alert("ERRO: Ficheiro não foi gravado!")
+            },
+            error: e => {
+                alert("Erro no post: " + JSON.stringify(e))
+                console.log("ERRO: " + e)
+            }
+        })
+        $("#files").val("")
+        $("#desc").val("")
+    }
+})
--- TP8/FileSubmission/routes/index.js
+++ TP8/FileSubmission/routes/index.js
@@ -1,9 +1,78 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+var jsonfile = require("jsonfile")
+var fs = require("fs")
+var formidable = require("formidable")
+var fsExtra = require("fs.extra")
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+var myBD = __dirname + "/../public/data/data.json" 
 
-module.exports = router;
+router.get('/', (req, res) => {res.render("index")})
+
+router.get("/table", (req,res) => {
+    jsonfile.readFile(myBD, (erro,dados)=>{
+        if(!erro) res.render("table", {ficheiros: dados})
+        else res.json(erro)
+    })
+})
+
+router.get("/uploaded/*",(req,res)=>{
+    jsonfile.readFile(myBD,(erro,dados)=>{
+        if(!erro){
+            var filepath = req.url
+            var type = dados.find(e => e.path==filepath).type
+            res.writeHead(200,{"Content-Type": type})
+            fs.readFile(filepath, (erro2,dados)=>{
+                if(!erro2) res.write(dados)
+                else res.json(erro2)
+            })
+        }else{
+            res.json(erro)
+        }
+    })
+})
+
+router.post("/saveFile",(req,res)=>{
+    var form = new formidable.IncomingForm()
+
+    form.parse(req, (erro, fields, formData)=>{
+        if(!erro){
+            var fenviado = formData.file.path
+            var fnovo = __dirname + "/../public/uploaded/" + formData.file.name
+
+            fsExtra.move(fenviado,fnovo, erro2 => {
+                if(!erro2){
+                    jsonfile.readFile(myBD,(erro3,dados)=>{
+                        if(!erro3){
+                            var fich = {nome: formData.file.name,
+                                        path: "/uploaded/" + formData.file.name,
+                                        desc: fields.desc,
+                                        type: formData.file.type}
+                            dados.push(fich)
+                            jsonfile.writeFile(myBD, dados, erro4 =>{
+                                if(!erro4){
+                                    console.log("Registo gravado com sucesso")
+                                    res.json(formData.file.name)
+                                }else{
+                                    console.log("Erro: " + erro4)
+                                    res.json("")
+                                }   
+                            })
+                        }else{
+                            console.log("Erro: " + erro3)
+                            res.json("")
+                        }   
+                    })
+                }else{
+                    console.log("Erro: " + erro2)
+                    res.json("")
+                }
+            })
+        }else{
+            console.log("Erro: " + erro)
+            res.json("")
+        }   
+    })
+})
+
+module.exports = router
